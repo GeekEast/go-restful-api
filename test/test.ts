dec91155ca95a2bdde26a56faf06e449902e67d3
GeekEast <xiang.tan.p2109@gmail.com>
2020-03-02 13:31:18 +1030

    init

---
+++ test/test.ts
@@ -0,0 +1,23 @@
+import acn from 'autocannon'
+
+const book = {
+  Isbn: "9984059",
+  Title: "Book",
+  Author: {
+    FirstName: "James",
+    LastName: "Tan"
+  }
+
+}
+const instance = acn({
+  url: 'http://localhost:8080/api/books/',
+  connections: 10,
+  duration: 10,
+  pipelining: 2,
+  method: "POST",
+  body: JSON.stringify(book)
+}, console.log)
+
+
+// just render results
+acn.track(instance, { renderProgressBar: true })
